9c5e616b6e1738de9bcc1851eee12bea98af5418
Eamonn <eamonnkillian@gmail.com>
2016-08-09 11:23:16 +0100

    Update default.rb

--- std_oracle_prerequisites/attributes/default.rb
+++ std_oracle_prerequisites/attributes/default.rb
@@ -1,2 +1,2 @@
-default['oraclefiles'] = "http://10.164.91.13/files/ORACLE/"
+default['oraclefiles'] = "http://<your servers IP>/files/ORACLE/"
 default['compatrpm'] = "compat-libstdc++-33-3.2.3-47.3.x86_64.rpm"
