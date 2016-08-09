d737712bd9e0700571ba6cd17b198930dec4b602
Eamonn <eamonnkillian@gmail.com>
2016-08-09 11:23:53 +0100

    Add files via upload

---
+++ std_devtools/metadata.rb
@@ -0,0 +1,7 @@
+name             'std_devtools'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures std_devtools'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
